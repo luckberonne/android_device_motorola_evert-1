f408b762eedc7a83f33c32080faeff81da7a2f0f
seba3567 <sebastian26481@gmail.com>
2019-09-01 14:02:13 -0400

    fix derps

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -3,14 +3,14 @@
 #
 
 # Audio
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.audio.calfile0=/vendor/etc/acdbdata/idn/Global_cal.acdb \
-    persist.vendor.audio.calfile1=/vendor/etc/acdbdata/idn/Bluetooth_cal.acdb \
-    persist.vendor.audio.calfile2=/vendor/etc/acdbdata/idn/General_cal.acdb \
-    persist.vendor.audio.calfile3=/vendor/etc/acdbdata/idn/Speaker_cal.acdb \
-    persist.vendor.audio.calfile4=/vendor/etc/acdbdata/idn/Hdmi_cal.acdb \
-    persist.vendor.audio.calfile5=/vendor/etc/acdbdata/idn/Headset_cal.acdb \
-    persist.vendor.audio.calfile6=/vendor/etc/acdbdata/idn/Handset_cal.acdb \
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    persist.vendor.audio.calfile0=/vendor/etc/acdbdata/idn/Global_cal.acdb \
+#    persist.vendor.audio.calfile1=/vendor/etc/acdbdata/idn/Bluetooth_cal.acdb \
+#    persist.vendor.audio.calfile2=/vendor/etc/acdbdata/idn/General_cal.acdb \
+#    persist.vendor.audio.calfile3=/vendor/etc/acdbdata/idn/Speaker_cal.acdb \
+#    persist.vendor.audio.calfile4=/vendor/etc/acdbdata/idn/Hdmi_cal.acdb \
+#    persist.vendor.audio.calfile5=/vendor/etc/acdbdata/idn/Headset_cal.acdb \
+#    persist.vendor.audio.calfile6=/vendor/etc/acdbdata/idn/Handset_cal.acdb \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
